41ccef8f6099a4fe049bfe2965825088fec66618
Yaojin Sun <sunyaojin2011@foxmail.com>
2017-11-01 19:56:52 -0400

    db test

--- rctoken/tests/RCT.tests/test_db_1.py
+++ rctoken/tests/RCT.tests/test_db_1.py
@@ -27,17 +27,18 @@ def generate_simplified_random_TX(number_of_TX):
 
 def main():
     (transaction_id, sender, receiver)= generate_simplified_random_TX(8)
-    print(utils.encode_hex(sender))
-    print(utils.encode_hex(receiver))
-    print(utils.encode_hex(transaction_id))
+    # print(utils.encode_hex(sender))
+    # print(utils.encode_hex(receiver))
+    # print(utils.encode_hex(transaction_id))
     reputation_db = redis.StrictRedis(host='localhost', port=6379, db=0)
     reputation_db.set(transaction_id, sender+receiver)
     my_decode_key = decode_hex('352f81d7e82301723bc56ce1d33e1a608e07f007e19f8e846a062336d64d544f')
     test_get_results = reputation_db.get(my_decode_key)
     print(utils.encode_hex(test_get_results))
+    # print(reputation_db.keys())
     for i in reputation_db.keys():
         print(utils.encode_hex(i))
-    print(sys.getsizeof(reputation_db))
+    # print(sys.getsizeof(reputation_db))
 
 
 if __name__ == '__main__':
